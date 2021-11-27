038e6f63a8d14b8ba9a1079850e07d861afb525c
Yaroslav <yaroslavmiloslavsky@gmail.com>
2021-11-27 15:07:24 +0200

    setup settings

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CellularAutomata',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='Free',
+    author='Yaroslav',
+    author_email='YaroslavMiloslavsky@gmail.com',
+    description='MMN11Q2'
+)
